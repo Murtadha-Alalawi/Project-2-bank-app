19b880c34fc6be164bf307d85143afba0fecf553
Murtadha-Alalawi <murtadha.alawi04@gmail.com>
2025-01-22 00:00:22 +0300

    create new.ejs form snd routes

--- controllers/bankApp.js
+++ controllers/bankApp.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index.ejs')
+const User = require('../models/user')
+
+
+
+router.get('/', async (req, res) => {
+   try {
+    const currentUser = await User.findById(req.session.user._id)
+
+    res.render('index.ejs',{application:currentUser.applications})
+   } catch (error) {
+    res.redirect('/')
+   }
 })
+
+
+router.get('/new', async (req,res)=>{
+    res.render('application/new.ejs')
+})
+
+
+router.post('/', async (req,res)=>{
+    try {
+        
+        const currentUser = await User.findById(req.session.user._id)
+
+        currentUser.applications.push(req.body)
+
+        await currentUser.save()
+
+        res.redirect(`/users/${currentUser._id}/applications`)
+
+        
+
+    } catch (error) {
+        res.redirect('/')
+    }
+})
+
+
 
 module.exports = router;
