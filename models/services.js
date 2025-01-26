446f39d23ccb385f32079fe54f1d9ba4b6473c9a
Loai-101 <q9g8moh@gmail.com>
2025-01-26 19:05:19 +0300

    services

--- models/services.js
+++ models/services.js
@@ -5,53 +5,9 @@ const depositSchema = require('./deposit.js')
 const withdrawSchema = require('./withdraw.js')
 const transferSchema = require('./transfer.js')
 
-const balanceSchema = new mongoose.Schema({
-    amount: {
-        type: Number,
-        required: true
-    }
-})
 
-const depositSchema = new mongoose.Schema({
-    amount: {
-        type: Number,
-        required: true
-    },
-    date: {
-        type: Date,
-        required: true
-    }
-})
 
-const transferSchema = new mongoose.Schema({
-    amount: {
-        type: Number, 
-        required: true},
-    from: {
-        type: applicationSchema,
-        required: true},
-    to: {
-        type: applicationSchema,
-        required: true},
-    date: {
-        type: Date, 
-        required: true},
-    description: {
-        type: String, 
-        required: true}
 
-})
-
-const withdrawSchema = new mongoose.Schema({
-    amount: {
-        type: Number,
-        required: true
-    },
-    date: {
-        type: Date,
-        required: true
-    }
-})
 
 const transactionSchema = new mongoose.Schema({
     type: {
